fe48976f959d3cb90c4fd9f8ba2ae48d9031fbf9
Madhur Bhargava <madhur.bhargava333@gmail.com>
2016-06-02 23:35:53 +0530

    Added Image Pickup from phone gallery and location extraction.

--- ImagePage.xaml.cs
+++ ImagePage.xaml.cs
@@ -6,7 +6,10 @@ using ExifLib;
 using System.IO;
 using Xamarin;
 using System.Reflection;
-
+using Plugin.Media;
+using PCLStorage;
+using Plugin.Geolocator;
+using System.Diagnostics;
 
 namespace XamarinFormsMapsDemo
 {
@@ -14,19 +17,20 @@ namespace XamarinFormsMapsDemo
 	{
 		double lat;
 		double lang;
+
 		public ImagePage ()
 		{
 			InitializeComponent ();
-
-			//ExifInterface exif = new ExifInterface(filepath);
-			//ImageSource sample = SampleImage.Source;
-
-			//byte[] data =  File.ReadAll("image.png");
-			//System.IO.Stream image = new StreamImageSource("sample.jpg");
-			//ExifReader.ReadJpeg(stream);
 			MapButton.Clicked +=  OnClicked;
+			PhotoButton.Clicked += OnPhotoClick;
+			GalleryButton.Clicked += OnPhotoPickup;
 		}
 
+		/// <summary>
+		/// Picks up the default bundled photo, extracts location and plots this on map.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">E.</param>
 		private void OnClicked(object sender, EventArgs e)
 		{
 			
@@ -48,6 +52,92 @@ namespace XamarinFormsMapsDemo
 			}
 
 			Navigation.PushModalAsync (new MapPage (lat, lang));
+		}
+
+		/// <summary>
+		/// This will launch photo gallery. Extracts location from photo and plots those on the map.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">E.</param>
+		private async void OnPhotoPickup(object sender, EventArgs e)
+		{
+			if (!CrossMedia.Current.IsPickPhotoSupported) {
+				//this.ShowUnsupportedMediaAlert ();
+				return;
+			}
+
+			var ImageFromLibrary = await CrossMedia.Current.PickPhotoAsync ();
+
+			if (ImageFromLibrary != null) {
+
+				IFile ImageFile = await FileSystem.Current.GetFileFromPathAsync (ImageFromLibrary.Path);
+
+				Stream stream = await ImageFile.OpenAsync (FileAccess.Read);
+
+				var JpegInfo = ExifReader.ReadJpeg (stream);
+
+
+				lat = JpegInfo.GpsLatitude[0] + JpegInfo.GpsLatitude[1] / 60 + JpegInfo.GpsLatitude[2] / 3600;//JpegInfo.GpsLatitude[0];
+				lang =  JpegInfo.GpsLongitude[0] + JpegInfo.GpsLongitude[1] / 60 + JpegInfo.GpsLongitude[2] / 3600;//JpegInfo.GpsLongitude[0];
+				ExifGpsLatitudeRef latRef = JpegInfo.GpsLatitudeRef;
+				ExifGpsLongitudeRef longRef = JpegInfo.GpsLongitudeRef;
+				if (latRef == ExifGpsLatitudeRef.South) {
+					lat = lat * -1;
+				}
+
+				if (longRef == ExifGpsLongitudeRef.West) {
+					lang = lang * -1;
+				}
+
+				await Navigation.PushModalAsync (new MapPage (lat, lang));
+			}
+
+
+
+
+		}
+
+		/// <summary>
+		/// Raises the photo click event. This will launch camera for photo click, extracts location and plots those on the map.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">E.</param>
+		private async void OnPhotoClick(object sender, EventArgs e)
+		{
+			if (!CrossMedia.Current.IsCameraAvailable) {
+				//this.ShowUnsupportedMediaAlert ();
+				return;
+			}
+
+			var locator = CrossGeolocator.Current;
+			locator.DesiredAccuracy = 50;
+
+			var position = await locator.GetPositionAsync (timeoutMilliseconds: 10000);
+
+			var ImageFilename = string.Format ("{0}.jpg", DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond);
+
+			var image = await CrossMedia.Current.TakePhotoAsync (new Plugin.Media.Abstractions.StoreCameraMediaOptions {
+				Directory = "../Library",
+				Name = ImageFilename
+			});
+			if (image != null) 
+			{
+
+				Stream stream = image.GetStream ();
+
+				IFile ImageFile = await FileSystem.Current.GetFileFromPathAsync (image.Path);
+
+				byte[] bytes;
+				using (var ms = new MemoryStream())
+				{
+					stream.CopyTo(ms);
+					bytes = ms.ToArray();
+				}
+
+
+
+				await Navigation.PushModalAsync (new MapPage (position.Latitude, position.Longitude));
+			}
 		}
 
 
