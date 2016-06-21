8532f41e653ca6dc11962c8ad3f9d8769f4ff301
Madhur Bhargava <madhur.bhargava333@gmail.com>
2016-06-21 23:53:31 +0530

    Added Utility Class

---
+++ Utility.cs
@@ -0,0 +1,34 @@
+﻿//
+// Utility.cs
+//
+// Author:
+//       madhur <>
+//
+// Copyright (c) 2016 madhur
+using System;
+using System.Text.RegularExpressions;
+
+namespace SomeNameSpace
+{
+	public class Utility
+	{
+
+		public static int MIN_PHONE_DIGITS = 9;
+		public static int MAX_PHONE_DIGITS = 13;
+
+
+		public Utility ()
+		{
+		}
+
+		public static bool IsPhoneNumberValid(String phone)
+		{
+			bool isPhoneValid = false;
+			if (phone != null && (phone.Length >= MIN_PHONE_DIGITS && phone.Length <= MAX_PHONE_DIGITS)) {
+				isPhoneValid = Regex.Match (phone, @"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$").Success;
+			}
+			return isPhoneValid;
+		}
+	}
+}
+
